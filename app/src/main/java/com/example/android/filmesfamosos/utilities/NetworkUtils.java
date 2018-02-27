2988c79fdabb750dc97e4a98789a3ae7acdd1a97
Guilherme Canalli <guilherme.canalli5@gmail.com>
2018-02-27 15:06:19 +0100

    Removed unused references

--- app/src/main/java/com/example/android/filmesfamosos/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/android/filmesfamosos/utilities/NetworkUtils.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.filmesfamosos.utilities;
 
 import android.content.Context;
@@ -28,9 +13,6 @@ import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.util.Scanner;
 
-/**
- * These utilities will be used to communicate with the weather servers.
- */
 public final class NetworkUtils {
     public static final String SORT_BY_POPULARITY = "popular";
     public static final String SORT_BY_TOP_RATED = "top_rated";
