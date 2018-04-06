c24daf89e11dae4badeb9e57fa02a719a4e31939
Guilherme Canalli da Silva <guilherme.canalli5@gmail.com>
2018-04-06 12:44:09 +0200

    Further improvements to movie detail layout

--- app/src/main/java/com/example/android/filmesfamosos/MovieDetailActivity.java
+++ app/src/main/java/com/example/android/filmesfamosos/MovieDetailActivity.java
@@ -3,7 +3,9 @@ package com.example.android.filmesfamosos;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.example.android.filmesfamosos.model.Movie;
@@ -14,20 +16,18 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MovieDetailActivity extends AppCompatActivity {
-
-    private ImageView mMoviePictureImageView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
 
-        mMoviePictureImageView = findViewById(R.id.iv_moviepicture);
+        ImageView mMoviePictureImageView = findViewById(R.id.iv_moviepicture);
         TextView mMovieTitleTextView = findViewById(R.id.tv_movietitle);
         TextView mMovieOriginalTitleTextView = findViewById(R.id.tv_original_title);
         TextView mMovieDescriptionTextView = findViewById(R.id.tv_moviesdescription);
         TextView mReleaseDateTextView = findViewById(R.id.tv_movie_releasedate);
         TextView mMovieRatingTextView = findViewById(R.id.tv_movierating);
+        ScrollView scrollView = findViewById(R.id.scrollview_movie_details);
 
         Intent intentFromMain = getIntent();
         if(intentFromMain.hasExtra(getString(R.string.intentres_name_movie))){
@@ -35,13 +35,13 @@ public class MovieDetailActivity extends AppCompatActivity {
             String miniatureURL = clickedMovie.getPosterPath();
             Picasso.with(this).load(miniatureURL).into(mMoviePictureImageView);
             mMovieTitleTextView.setText(clickedMovie.getTitle().getTitle());
-            String originalTitle = getString(R.string.moviedetail_menu_original_name) + clickedMovie.getTitle().getOriginalTitle();
+            String originalTitle = clickedMovie.getTitle().getOriginalTitle();
             mMovieOriginalTitleTextView.setText(originalTitle);
             mMovieDescriptionTextView.setText(clickedMovie.getOverview());
             try {
                 Date date = new SimpleDateFormat(getString(R.string.movieapi_date_format)).parse(clickedMovie.getReleaseDate());
                 String formattedDate = new SimpleDateFormat(getString(R.string.date_short_date_format)).format(date);
-                String releaseDateWithLabel = getString(R.string.moviedetail_menu_release_date) + formattedDate;
+                String releaseDateWithLabel = formattedDate;
                 mReleaseDateTextView.setText(releaseDateWithLabel);
             } catch (ParseException e) {
                 e.printStackTrace();
